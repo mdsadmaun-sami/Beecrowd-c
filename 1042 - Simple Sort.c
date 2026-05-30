4e2db5cbf96e54b99b071fcbdbe4e1d6543835b0
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-05-30 23:49:01 +0600

    Add Simple Sort program in C

---
+++ 1042 - Simple Sort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    int x, y, z;
+    int temp;
+    scanf("%d %d %d", &a, &b, &c);
+    x = a;
+    y = b;
+    z = c;
+    if (a > b) 
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a > c) 
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b > c) 
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    printf("%d\n%d\n%d\n", a, b, c);
+    printf("\n");
+    printf("%d\n%d\n%d\n", x, y, z);
+    return 0;
+}
